93176df5f971326ec42f2e8d9d9b3576bc1b8b53
V10cio <161033040+V10cio@users.noreply.github.com>
2024-09-12 20:30:50 +0700

    API
    belajar API dan method2nya

---
+++ app-kitobiso/routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\DonationController;
+use App\Http\Controllers\FundingController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/menu',function(Request $request){
+    return response()->json(['Home','Profile','About','Contact Us']);
+});
+
+//buat route menuju url/donatur
+//buat response berupa data json seperti berikut
+
+Route::get('/donatur',function(Request $request){
+    return response()->json(
+        [
+            ['id'=> 1, 'nama'=> 'Donatur 1'],
+            ['id'=> 2, 'nama'=> 'Donatur 2'],
+            ['id'=> 3, 'nama'=> 'Donatur 3'],
+        ]
+    );
+});
+
+//API CRUD Funding
+
+Route::get('/funding',[FundingController::class,'index']); //get all data
+Route::post('/funding',[FundingController::class,'store']); //create new data
+Route::get('/funding{id}',[FundingController::class,'show']); //create new data
+Route::put('/funding{id}',[FundingController::class,'update']); //create new data
+Route::delete('/funding{id}',[FundingController::class,'destroy']); //create new data
+
+// API CRUD DONATION
+Route::get('/donation',[DonationController::class,'index']);
+Route::apiResource('donation',DonationController::class);
