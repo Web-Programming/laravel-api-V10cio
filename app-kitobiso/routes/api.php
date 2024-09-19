efad95ac28882910074567af2ae5b02e3e3e39dc
V10cio <161033040+V10cio@users.noreply.github.com>
2024-09-19 20:31:00 +0700

    update auth user api

--- app-kitobiso/routes/api.php
+++ app-kitobiso/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DonationController;
 use App\Http\Controllers\FundingController;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,31 +11,40 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/menu',function(Request $request){
-    return response()->json(['Home','Profile','About','Contact Us']);
+Route::get('/menu', function (Request $request) {
+    return response()->json(['Home', 'Profile', 'About', 'Contact Us']);
 });
 
 //buat route menuju url/donatur
 //buat response berupa data json seperti berikut
 
-Route::get('/donatur',function(Request $request){
+Route::get('/donatur', function (Request $request) {
     return response()->json(
         [
-            ['id'=> 1, 'nama'=> 'Donatur 1'],
-            ['id'=> 2, 'nama'=> 'Donatur 2'],
-            ['id'=> 3, 'nama'=> 'Donatur 3'],
+            ['id' => 1, 'nama' => 'Donatur 1'],
+            ['id' => 2, 'nama' => 'Donatur 2'],
+            ['id' => 3, 'nama' => 'Donatur 3'],
         ]
     );
 });
 
-//API CRUD Funding
 
-Route::get('/funding',[FundingController::class,'index']); //get all data
-Route::post('/funding',[FundingController::class,'store']); //create new data
-Route::get('/funding{id}',[FundingController::class,'show']); //create new data
-Route::put('/funding{id}',[FundingController::class,'update']); //create new data
-Route::delete('/funding{id}',[FundingController::class,'destroy']); //create new data
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    //API CRUD Funding
+    Route::get('/funding', [FundingController::class, 'index']); //get all data
+    Route::post('/funding', [FundingController::class, 'store']); //create new data
+    Route::get('/funding{id}', [FundingController::class, 'show']); //create new data
+    Route::put('/funding{id}', [FundingController::class, 'update']); //create new data
+    Route::delete('/funding{id}', [FundingController::class, 'destroy']); //create new data
 
-// API CRUD DONATION
-Route::get('/donation',[DonationController::class,'index']);
-Route::apiResource('donation',DonationController::class);
+    // API CRUD DONATION
+    Route::get('/donation', [DonationController::class, 'index']);
+    Route::apiResource('donation', DonationController::class);
+
+
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
